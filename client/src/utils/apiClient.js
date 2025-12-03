9a749bd0a5720393c7c64767d54f824fa0b1ad4a
AnuruddhSin <anuruddh7234@gmail.com>
2025-12-03 17:17:42 +0530

    Stats

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -2,5 +2,9 @@ import axios from "axios";
 import { API_BASE_URL } from "./constants";
 
 export const api = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: API_BASE_URL,
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
